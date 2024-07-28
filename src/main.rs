2b1e7e0d41662f14ff07c86b6e344f97b295fe05
sebastian Whiffen <sebastianwhiffen@gmail.com>
2024-07-28 20:11:42 +1000

    learning

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,18 @@
 use bevy::{prelude::*, DefaultPlugins};
 
+mod systems;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(
             Startup,
-            (spawn_player, spawn_camera, spawn_floor, spawn_light),
+            (spawn_player, systems::camera::spawn_camera, systems::level::spawn_floor, spawn_light),
         )
         .run();
 }
 
-fn spawn_camera(mut commands: Commands) {
-    let camera = Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    };
-    commands.spawn(camera);
-}
 
-fn spawn_floor(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let floor = PbrBundle {
-        mesh: meshes.add(Mesh::from(Plane3d::default().mesh().size(15.0, 15.0))),
-        material: materials.add(Color::srgb(0.0, 1.0, 0.0)),
-        ..default()
-    };
-
-    commands.spawn(floor);
-}
 
 fn spawn_player(
     mut commands: Commands,
---
+++ src/systems/level.rs
@@ -0,0 +1,15 @@
+use bevy::prelude::*;
+
+pub fn spawn_floor(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let floor = PbrBundle {
+        mesh: meshes.add(Mesh::from(Plane3d::default().mesh().size(15.0, 15.0))),
+        material: materials.add(Color::srgb(0.0, 1.0, 0.0)),
+        ..default()
+    };
+
+    commands.spawn(floor);
+}
